341e59b0cebecd2fcf2e7bcbd3041f9dedccf096
jessen gibbs <jessen.gibbs@mail.mcgill.ca>
2020-08-24 13:19:39 -0400

    changes to create post modal

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8e451a7d921fdef62f1a";
+/******/ 	var hotCurrentHash = "2e6fb518b10089324359";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
